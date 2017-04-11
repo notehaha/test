28f6de569580c7e4884266f68764d3c26a23c1d9
Niti Chivaprasertporn <notehaha55@gmail.com>
2017-04-11 17:13:07 +0700

    Initial Commit

---
+++ WishAnyware/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WishAnyware
+//
+//  Created by Niti Chivaprasertporn on 4/11/2560 BE.
+//  Copyright © 2560 Niti Chivaprasertporn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
